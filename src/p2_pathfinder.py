07e036f156a0dc869c12fb291401280c1f3063f5
Teijien <43975613+Teijien@users.noreply.github.com>
2018-10-12 21:10:15 -0700

    Update src/p2_pathfinder.py

--- src/p2_pathfinder.py
+++ src/p2_pathfinder.py
@@ -1,6 +1,6 @@
 from p1 import dijkstras_shortest_path, navigation_edges
-from math import inf, sqrt
 from heapq import heappop, heappush
+from math import inf, sqrt
 
 def find_path (source_point, destination_point, mesh):
 
@@ -18,87 +18,164 @@ def find_path (source_point, destination_point, mesh):
         A list of boxes explored by the algorithm
     """
 
-    print(mesh['adj'])
-    print(source_point)
+    path = []
+    boxes = {}
 
-    # Pseudocode found at: https://en.wikipedia.org/wiki/A*_search_algorithm (10/12/18)
-
-    evaluated = [] # set of black nodes
-    queue = [(0,source_point)] # priority queue of nodes to look at
-    parent = {} # dict containing child nodes with parent as key
-    distances = {source_point: 0} # dict containing distance from start to key node
-    heuristic = {source_point: pythagorean(source_point[1]-destination_point[1], source_point[0]-destination_point[0])}
-    # dict containing distance from start node to key node + Euclidean distance from key node to end node
-
-    while len(queue) > 0:
-        currentNode = heappop(queue) # take the node with least distance off the queue for examination
-
-        if currentNode == destination_point: # if it's the node we're looking for, compute and return the path
-            return form_path(parent, currentNode)
-
-        evaluated.append(currentNode) # add the current node to the list of black nodes
-
-        for adjacentNode in mesh['adj'][currentNode]: # looks at each node in adjacency list of current node
-            
-            if adjacentNode in evaluated: # if the node is black, ignore it
-                continue
-
-            else: # otherwise, try to calculate a better heuristic
-                tempHeuristic = heuristic[currentNode] + pythagorean((currentNode[1]-adjacentNode[1]), (currentNode[0]-adjacentNode[0]))
-
-                if not adjacentNode in queue: # node has been discovered
-                    heappush(queue, adjacentNode)
-
-                elif adjacentNode in heuristic.keys() and tempHeuristic >= heuristic[adjacentNode]: # the heuristic is not better
-                    continue
-
-            # record best path
-            parent[adjacentNode] = currentNode
-            heuristic[adjacentNode] = tempHeuristic
-            distances[adjacentNode] = heuristic[adjacentNode] + pythagorean((adjacentNode[1]-destination_point[1]), (adjacentNode[0]-destination_point[0]))
-
-"""
     # Print source and destination coordinates
     print("Source coordinates: ", source_point, "\n")
     print("Destination coordinates: ", destination_point, "\n")
 
-    # Source Coordinates
-    sx = source_point[1]
-    sy = source_point[0]
+    points, boxes = dijkstras_shortest_path(source_point, destination_point, mesh)
+    
+    for i in range(len(points) - 1):
+        print(points[i], points[i+1])
+        path.append((points[i], points[i + 1]))
 
-    # Destination Coordinates
-    dx = destination_point[1]
-    dy = destination_point[0]
-
-        # Iterate through mesh boxes and put them in the dictionary: boxes.
-        # Prints out the source and destination box if found.
-
-        # Currently sets all box values in the dictionary to None
-    for box in mesh['boxes']:
-        # Box x Coordinates
-        x1 = box[2]
-        x2 = box[3]
-
-        # Box y Coordinates
-        y1 = box[0]
-        y2 = box[1]
-
-        # Initialize all box values to their adjacency list
-        boxes[box] = mesh['adj'][box]
-
-        # Check if equal to source or destination box and append to path, then print out.
-        if sx >= x1 and sx < x2:
-            if sy >= y1 and sy < y2:
-                print("Source Box: ", box, "\n")
-        if dx >= x1 and dx < x2:
-            if dy >= y1 and dy < y2:
-                print("Destination Box: ", box, "\n")
-
-    #print(boxes)
-    #print(mesh)
+    print(path)
 
     return path, boxes.keys()
+
+""" Finds the box accociated with a point
 """
+def find_box(point, boxes, visited):
+    for box in boxes:
+        #print(point)
+        #print(box)
+        if point[1] >= box[2] and point[1] <= box[3]:
+            if point[0] >= box[0] and point[0] <= box[1]:
+                if box != visited:
+                    #print("found")
+                    return box
+    print("none found")
+    return None
+
+def dijkstras_shortest_path(initial_position, destination, mesh):
+    """ Searches for a minimal cost path through a graph using BFS algorithm.
+
+    Args:
+        initial_position: The initial box from which the path extends.
+        destination: The end location for the path.
+        adj: An adjacency dictionary containing boxes adjacent to a given box.
+
+    Returns:
+        If a path exits, return a list containing all cells from initial_position to destination.
+        Otherwise, return None.
+
+    """
+    # The priority queue
+    queue = [(0, initial_position)]
+
+    # The dictionary that will be returned with the costs
+    distances = {initial_position: 0}
+
+    # The dictionary holding the boxes we've traversed
+    boxes = {}
+
+    # The dictionary holding the heuristic
+    heuristic = {initial_position: pythagorean(initial_position[1] - destination[1], initial_position[0] - destination[0])}
+
+    # The dictionary holding backpointers
+    backpointers = {initial_position: None}
+
+    current_box = None
+
+    destination_node = find_box(destination, mesh['boxes'], current_box)
+
+    while queue:
+        print("queue: ", queue)
+        current_dist, current_point = heappop(queue)
+        #print(current_point)
+        #print(current_dist)
+        current_node = find_box(current_point, mesh['boxes'], current_box)
+        #print(current_node)
+
+        boxes[current_node] = mesh['adj'][current_node]
+
+        if current_node == destination_node:
+            pathcost = current_dist + pythagorean(current_point[1] - destination[1], current_point[0] - destination[0])
+            distances[destination] = pathcost
+            heuristic[destination] = 0
+            backpointers[destination] = current_point
+
+            current_point = destination
+
+            # List containing all cells from initial_position to destination
+            path = [current_point]
+
+            # Go backwards from destination until the source using backpointers
+            # and add all the nodes in the shortest path into a list
+            current_back_point = backpointers[current_point]
+            while current_back_point is not None:
+                path.append(current_back_point)
+                current_back_point = backpointers[current_back_point]
+
+            return path[::-1], boxes
+
+        # Calculate cost from current note to all the adjacent ones
+        for adj_node, adj_node_cost in navigation_edges(boxes[current_node], current_point):
+            pathcost = current_dist + adj_node_cost
+
+            # If the cost is new
+            if adj_node not in distances or pathcost < distances[adj_node]:
+                distances[adj_node] = pathcost
+                heuristic[adj_node] = pathcost + pythagorean(adj_node[1] - destination[1], adj_node[0] - destination[0])
+                backpointers[adj_node] = current_point
+                heappush(queue, (heuristic[adj_node], adj_node))
+                current_box = current_node
+
+    return None
+
+def navigation_edges(level, cell):
+    """ Provides a list of adjacent cells and their respective costs from the given cell.
+
+    Args:
+        level: A list of adjacent cells to the current cell
+        cell: A target location.
+
+    Returns:
+        A list of tuples containing an adjacent cell's coordinates and the cost of the edge joining it and the
+        originating cell.
+
+        E.g. from (0,0):
+            [((0,1), 1),
+             ((1,0), 1),
+             ((1,1), 1.4142135623730951),
+             ... ]
+    """
+
+    detail_points = {}
+
+    for box in level:
+        #print(box)
+        y1, y2, x1, x2 = box
+
+        if cell[0] >= y1 and cell[0] <= y2:
+            if cell[1] < x1:
+                detail_points[cell[0], x1] = pythagorean(cell[1] - x1, 0)
+            elif cell[1] > x2:
+                detail_points[cell[0], x2] = pythagorean(cell[1] - x2, 0)
+
+        elif cell[1] >= x1 and cell[1] <= x2:
+            if cell[0] < y1:
+                detail_points[y1, cell[1]] = pythagorean(0, cell[0] - y1)
+            elif cell[0] > y2:
+                detail_points[y2, cell[1]] = pythagorean(0, cell[0] - y2)
+
+        else:
+            temp_heap = []
+            for x in (x1, x2):
+                for y in (y1, y2):
+
+                    next_point = x, y
+                    heappush(temp_heap, (pythagorean(cell[1] - x, cell[0] - y), (y, x)))
+
+            #print("temp heap: ", temp_heap)
+            cost, new_point = heappop(temp_heap)
+            detail_points[new_point] = cost
+
+        #print(detail_points.items())
+
+    return detail_points.items()
 
 # uses Pythagorean Theorem to calculate straght-line dist between two Cartesian coordinates
 def pythagorean (a, b):
